740f09d92bfc0f96eae7f9d276e7f3c7789f1072
William Jackson <will@kstechnologies.com>
2014-07-30 20:08:26 -0600

    Initial Commit
    Copied in Nordic SDK and Example Template Project
    Made all necessary adjustments to the Template Project to allow it to compiled in place.
    Tested to flash on the Nordic nRF6310.

---
+++ Senior Design Hub/ble_app_template/arm/_build/ble_debug_assert_handler.d
@@ -0,0 +1,14 @@
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Source\ble\ble_debug_assert_handler.c
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\ble\ble_debug_assert_handler.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\nrf51.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\CMSIS\Include\core_cm0.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\system_nrf51.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\ble\ble_error_log.h
+.\_build\ble_debug_assert_handler.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\ble\ble_nrf6310_pins.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\ble\ble_flash.h
+.\_build\ble_debug_assert_handler.o: ..\..\..\NordicSDK\Include\nordic_common.h
