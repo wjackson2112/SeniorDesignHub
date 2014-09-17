ae18d02c76411eb5bb4d7afd970cb41d9eaf11ec
William Jackson <will@kstechnologies.com>
2014-09-17 17:42:45 -0600

    Added Global config file

---
+++ Senior Design Hub/ble_app_template/global_config.h
@@ -0,0 +1,18 @@
+#ifndef GLOBAL_CONFIG_H__
+#define GLOBAL_CONFIG_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/**********************
+ * UART CONFIGURATION *
+ **********************/
+#define UART_RTS_PIN_NUMBER 10
+#define UART_TXD_PIN_NUMBER 9
+#define UART_CTS_PIN_NUMBER 6
+#define UART_RXD_PIN_NUMBER 11
+#define UART_HWFC false
+#define UART_BAUD_RATE 4 //UART_BAUDRATE_BAUDRATE_Baud9600
+#define UART_PARITY_INCLUDED false
+
+#endif
