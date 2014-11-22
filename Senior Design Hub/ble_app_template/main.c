291423bcd22cb49758bc8a7b73a78d0873945ce2
William Jackson <will@kstechnologies.com>
2014-11-22 13:35:08 -0700

    Fixed RSSI loss problem and added ability to add all five services.

--- Senior Design Hub/ble_app_template/main.c
+++ Senior Design Hub/ble_app_template/main.c
@@ -59,33 +59,34 @@
 
 
 
-#define APP_ADV_INTERVAL                64                                          /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
-#define APP_ADV_TIMEOUT_IN_SECONDS      0                                         /**< The advertising timeout (in units of seconds). */
+#define APP_ADV_INTERVAL                     160                                        /**< The advertising interval (in units of 0.625 ms. This value corresponds to 25 ms). */
+#define APP_ADV_TIMEOUT_IN_SECONDS           0                                       /**< The advertising timeout in units of seconds. */
 
-// YOUR_JOB: Modify these according to requirements.
-#define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
-#define APP_TIMER_MAX_TIMERS            4                                           /**< Maximum number of simultaneously created timers. */
-#define APP_TIMER_OP_QUEUE_SIZE         5                                           /**< Size of timer operation queues. */
+#define CONFIG_BUTTON_PIN										 3
 
-#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(50, UNIT_1_25_MS)            /**< Minimum acceptable connection interval (0.5 seconds). */
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)           /**< Maximum acceptable connection interval (1 second). */
-#define SLAVE_LATENCY                   0                                           /**< Slave latency. */
-#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds). */
-#define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(20000, APP_TIMER_PRESCALER) /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (15 seconds). */
-#define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER)  /**< Time between each call to sd_ble_gap_conn_param_update after the first (5 seconds). */
-#define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
+#define APP_TIMER_PRESCALER                  0                                         /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_MAX_TIMERS                 4                                         /**< Maximum number of simultaneously created timers. */
+#define APP_TIMER_OP_QUEUE_SIZE              5                                         /**< Size of timer operation queues. */
 
-#define APP_GPIOTE_MAX_USERS            1                                           /**< Maximum number of users of the GPIOTE handler. */
+#define SECOND_1_25_MS_UNITS                 800                                       /**< Definition of 1 second, when 1 unit is 1.25 ms. */
+#define SECOND_10_MS_UNITS                   100                                       /**< Definition of 1 second, when 1 unit is 10 ms. */
+#define MIN_CONN_INTERVAL                    (SECOND_1_25_MS_UNITS / 2)                /**< Minimum acceptable connection interval (0.5 seconds), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL                    (SECOND_1_25_MS_UNITS)                    /**< Maximum acceptable connection interval (1 second), Connection interval uses 1.25 ms units. */
+#define SLAVE_LATENCY                        0                                         /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                     (4 * SECOND_10_MS_UNITS)                  /**< Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units. */
 
-#define BUTTON_DETECTION_DELAY          APP_TIMER_TICKS(50, APP_TIMER_PRESCALER)    /**< Delay from a GPIOTE event until a button is reported as pushed (in number of timer ticks). */
+#define FIRST_CONN_PARAMS_UPDATE_DELAY       APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER)/**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
+#define NEXT_CONN_PARAMS_UPDATE_DELAY        APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER)/**< Time between each call to sd_ble_gap_conn_param_update after the first (30 seconds). */
+#define MAX_CONN_PARAMS_UPDATE_COUNT         3                                         /**< Number of attempts before giving up the connection parameter negotiation. */
 
-#define SEC_PARAM_TIMEOUT               30                                          /**< Timeout for Pairing Request or Security Request (in seconds). */
-#define SEC_PARAM_BOND                  1                                           /**< Perform bonding. */
-#define SEC_PARAM_MITM                  0                                           /**< Man In The Middle protection not required. */
-#define SEC_PARAM_IO_CAPABILITIES       BLE_GAP_IO_CAPS_NONE                        /**< No I/O capabilities. */
-#define SEC_PARAM_OOB                   0                                           /**< Out Of Band data not available. */
-#define SEC_PARAM_MIN_KEY_SIZE          7                                           /**< Minimum encryption key size. */
-#define SEC_PARAM_MAX_KEY_SIZE          16                                          /**< Maximum encryption key size. */
+#define SEC_PARAM_TIMEOUT                    30                                        /**< Timeout for Pairing Request or Security Request (in seconds). */
+#define SEC_PARAM_BOND                       1                                         /**< Perform bonding. */
+#define SEC_PARAM_MITM                       0                                         /**< Man In The Middle protection not required. */
+#define SEC_PARAM_IO_CAPABILITIES            BLE_GAP_IO_CAPS_NONE                      /**< No I/O capabilities. */
+#define SEC_PARAM_OOB                        0                                         /**< Out Of Band data not available. */
+#define SEC_PARAM_MIN_KEY_SIZE               7                                         /**< Minimum encryption key size. */
+#define SEC_PARAM_MAX_KEY_SIZE               16                                        /**< Maximum encryption key size. */
+
 
 #define DEAD_BEEF                       0xDEADBEEF                                  /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
@@ -229,7 +230,7 @@ static void gap_params_init(void)
 
     err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
 		
-		//sd_ble_gap_tx_power_set(+4);
+		err_code = sd_ble_gap_tx_power_set(+4);
     APP_ERROR_CHECK(err_code);
 }
 
@@ -287,7 +288,7 @@ static void services_init(void)
     ble_conf_init(&m_conf, &conf_init);
 	
 	  // Initialize UART Service
-	  /*ble_uart_init_t uart_init;
+	  ble_uart_init_t uart_init;
 
     memset(&uart_init, 0, sizeof(uart_init));
 
@@ -298,7 +299,7 @@ static void services_init(void)
         uart_init.initial_transmit_state[i] = 0x00;
     }
 
-    ble_uart_init(&m_uart, &uart_init);*/
+    ble_uart_init(&m_uart, &uart_init);
 		
 		// Initialize I2C Service
 	  ble_i2c_init_t i2c_init;
@@ -315,7 +316,7 @@ static void services_init(void)
     ble_i2c_init(&m_i2c, &i2c_init);
 		
 		// Initialize Digital Service
-	  /*ble_dig_init_t dig_init;
+	  ble_dig_init_t dig_init;
     
     memset(&dig_init, 0, sizeof(dig_init));
 
@@ -326,10 +327,10 @@ static void services_init(void)
         dig_init.initial_transmit_state[i] = 0x00;
     }
 
-    ble_dig_init(&m_dig, &dig_init);*/
+    ble_dig_init(&m_dig, &dig_init);
 		
 		// Initialize SPI Service
-	  /*ble_spi_init_t spi_init;
+	  ble_spi_init_t spi_init;
 
     memset(&spi_init, 0, sizeof(spi_init));
 
@@ -341,7 +342,7 @@ static void services_init(void)
 				spi_init.initial_receive_state[i] = 0x00;
     }
 
-    ble_spi_init(&m_spi, &spi_init);*/
+    ble_spi_init(&m_spi, &spi_init);
 		
 		// Initialize Analog Service
 		/*ble_ana_init_t ana_init;
@@ -419,9 +420,9 @@ static void conn_params_init(void)
     cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
     cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
     cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
-    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = false;
-    cp_init.evt_handler                    = on_conn_params_evt;
+    cp_init.start_on_notify_cccd_handle    = m_conf.enter_passcode_handles.cccd_handle;
+    cp_init.disconnect_on_fail             = true;
+    cp_init.evt_handler                    = NULL;
     cp_init.error_handler                  = conn_params_error_handler;
     
     err_code = ble_conn_params_init(&cp_init);
@@ -1379,21 +1380,24 @@ static void spi_init(void){
 int main(void)
 {
     // Initialize
+	
     leds_init();
     timers_init();
+	
+		nrf_gpio_cfg_input(20, NRF_GPIO_PIN_PULLDOWN);
+		
     //gpiote_init();
     //buttons_init();
 		
 		flash_init();
 	
     ble_stack_init();
+		gap_params_init();
 		services_init();
+		conn_params_init();
+		sec_params_init();
+		advertising_init();
     scheduler_init();
-    gap_params_init();
-    advertising_init();
-    
-    conn_params_init();
-    sec_params_init();
 
 		uart_init();
 		i2c_init();
